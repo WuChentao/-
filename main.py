fb7e4bf8aad1d29e3b118d254ee23f117731bc44
吴陈涛 <2049718055@qq.com>
2022-08-22 12:56:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-time=localtime = time.localtime(time.time())
-data = {"time":{"value":time},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+localtime = time.localtime(time.time())
+data = {"time":{"value":localtime},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
