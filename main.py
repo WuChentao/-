2cfb0fbfc9a22b6376cbad69684055f0b96e4658
吴陈涛 <2049718055@qq.com>
2022-08-22 14:56:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,9 +48,10 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
+week_list = ["一","二","三","四","五","六","日"]
 data = {
   "today":{"value":today.strftime('%Y-%m-%d'),"color":get_random_color()},
-  "week":{"value":today.isoweekday()},
+  "week":{"value":week_list[today.strftime('%Y-%m-%d').weekday()]},
   "city":{"value":city,"color":get_random_color()},
   "weather":{"value":wea,"color":get_random_color()},
   "temperature":{"value":temperature,"color":get_random_color()},
